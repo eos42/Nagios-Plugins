95ff0cd0ade363942c304a39d8dae5ea74b4849f
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2018-06-07 10:51:03 +0100

    Create check_producing.sh

---
+++ check_producing.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+PATH=/usr/local/nagios/
+FILEDIR=/usr/local/nagios/unpaid_blocks.temp
+CLEOS=/opt
+
+if [[ $1 -eq 1 ]]; then
+        echo "Creating blank unpaid_blocks.temp..."
+        "-1">$FILEDIR
+fi
+
+OLD=$(<$FILEDIR)
+echo "Old unpaid_blocks: $OLD"
+UNPAID="$(/bin/bash $CLEOS/cleos.sh get table eosio eosio producers -l 150 | /bin/grep -A 6 "eos42freedom" | /bin/grep unpaid_blocks | /bin/grep -oP '(?<= )[0-9]+')"
+echo $UNPAID > $FILEDIR
+
+if [[ $UNPAID -eq $OLD ]];
+then
+  echo "Not Producing"
+  exit 1
+else
+  echo "Successfully Producing" >&2
+  exit 0
+fi
