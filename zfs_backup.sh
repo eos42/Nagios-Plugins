569340127b17ddc14e13c6b3a37ee39d8d1a29bb
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2018-07-13 22:37:00 +0100

    Create zfs_backup.sh

---
+++ zfs_backup.sh
@@ -0,0 +1,16 @@
+#!/bin/bash 
+
+# Set the PAST time to check against
+PAST=$(date -d "2 hours ago" +'%Y%m%d%H')
+
+# Get timestamp of last backup for comparison
+LAST_BACKUP=$(sudo zfs list -t snapshot | tail -1 | cut -d' ' -f1 | cut -d'@' -f2)
+
+
+if [ $PAST -gt $LAST_BACKUP ]; then
+        echo "ZFS Snapshots have stopped working"
+        exit 2
+else
+        echo "ZFS Snaptshots are workoing fine."
+        exit 0
+fi
