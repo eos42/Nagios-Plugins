948d0757310b09df3c58914b5b012e1755c14f2c
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2018-12-17 12:43:46 +0000

    Create chinai-cpu.sh

---
+++ chinai-cpu.sh
@@ -0,0 +1,33 @@
+DIR=/Ghostbusters/ghostbusters-eos42freedom
+CLEOS=$DIR/cleos.sh
+
+#Set max CPU average to compare against
+MAX=30
+
+#Create blank file
+echo -n >CPUSTATS
+
+
+end=$((SECONDS+60))
+while [ $SECONDS -lt $end ]; do   # Loop until interval has elapsed.
+
+        CPUA=$($CLEOS get account chintailease -j | jq -r ".cpu_limit.available")
+        CPUU=$($CLEOS get account chintailease -j | jq -r ".cpu_limit.used")
+        CPUR="(($CPUU/$CPUA)*100)"
+        CPURA=$(echo "scale=4; ${CPUR}" | bc | cut -d'.' -f 1 | sed 's/ //g')
+        # Append each line to CPUSTATS file
+        echo $CPURA>>CPUSTATS
+
+done
+
+#Compute the average
+AVG=$(awk '{s+=$1} END {print s/NR}' CPUSTATS)
+
+
+if (( $(echo "$AVG > $MAX" |bc -l) )); then
+  echo "Chintai CPU is above $MAX usage, currently averaging $AVG usage | Chintaileasecpu $AVG "
+  exit 2
+else
+  echo "Chintai CPU is below $MAX usage, currently averaging $AVG usage | Chintaileasecpu $AVG"
+  exit 0
+fi
