fa8b131f5a0d0191a6336d59be4b9c77b369dcf1
Charles Holtzkampf <charles.holtzkampf@gmail.com>
2019-01-17 15:59:23 +0000

    Update check_last_claim.sh

--- check_last_claim.sh
+++ check_last_claim.sh
@@ -7,18 +7,22 @@
 #
 ################################################################################
 DIR=/opt
-PRODUCER={add your producer}
-producer_state=$($DIR/./cleos.sh get table eosio eosio producers -l 1 -k owner -L eos42freedom)
-TIMESTAMP=$(echo $producer_state | jq -r '.rows[0].last_claim_time')
-let LAST_CLAIM_TIME=$TIMESTAMP/1000000
-LAST_CLAIM_TIMESTAMP=$(date -d @$LAST_CLAIM_TIME)
-CURRENT_TIME=$(date +%s)
-let LAST_CLAIM_TIME_SECONDS=$CURRENT_TIME-$LAST_CLAIM_TIME
+PRODUCER=eos42freedom
 DAY=86400
-if [ $LAST_CLAIM_TIME_SECONDS -gt $DAY ]; then
+
+producer_state=$($DIR/./cleos.sh get table eosio eosio producers -l 1 -k owner -L eos42freedom)
+LAST_CLAIM=$(echo $producer_state | jq -r '.rows[0].last_claim_time')
+
+# Calculate diff
+CLAIM_TIME=$(date -d "$LAST_CLAIM" +"%s")
+NOW=$(date +"%s")
+DIFF="$(($NOW-$CLAIM_TIME))"
+
+
+if [ $DIFF -gt $DAY ]; then
         echo "Your last claim was over 24 hours ago"
         exit 2
 else
-        echo "Last claim action was on ${LAST_CLAIM_TIMESTAMP}"
+        echo "Last claim action was on ${LAST_CLAIM}"
         exit 0
 fi
